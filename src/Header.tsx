b75ca01113cbbadad16d234c118d2af13db5ce47
Luke-Knaap <Luke.vd.knaap@gmail.com>
2024-09-14 17:24:03 +0200

    Added a icon. Added links. Updated packages. Updated CSS.

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,19 @@
+import {NavLink } from 'react-router-dom'
 import styles from './Header.module.css'
 function Header() {
     return (
         <header className={styles.header}>
             <h1>React Oefenen</h1>
             <div className={styles.info}>
-                <li> <a href="">Home</a></li>
-                <li> <a href="">About</a></li>              
-                <li> <a href="">Contact</a></li>
-                <li> <a href="">Hallo</a></li>
+                <li><NavLink to= "/" className={({isActive}) => isActive ?
+                styles.activeLink : undefined}>Home
+                </NavLink></li>
+                <li><NavLink to= "/about" className={({isActive}) => isActive ?
+                styles.activeLink : undefined}>About
+                </NavLink></li>
+                <li><NavLink to= "/contact" className={({isActive}) => isActive ?
+                styles.activeLink : undefined}>Contact
+                </NavLink></li>   
             </div>
             <hr className={styles.line}/>
         </header>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './Header.tsx'
 import './index.css'
+import About from './Link-Pages/About.tsx'
 import Header from './Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
+      <Router>
       <Header />
+      <Routes>
+        <Route path='/' element={<main/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+      </Router>
     </StrictMode>,
 )
